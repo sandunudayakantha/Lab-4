9ac1f2f0fb45618b3b9b29736d213b7ac8c8bbd0
it23305700s <168615334+it23305700s@users.noreply.github.com>
2026-02-22 13:53:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const condition = true;
 
 const myPromise = new Promise((resolve, reject) => {
  if (condition) {
- resolve('Success!222222222');
+ resolve('Success!2222');
  } else {
  reject('Failure!');
  }
